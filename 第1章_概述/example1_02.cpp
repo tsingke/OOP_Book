50385a75676c8ffab294fb5666a3511079e00c57
tsingke <tsingke@hotmail.com>
2019-09-24 10:56:33 +0800

    update the chapter's name

--- 第1章_概述/example1_02.cpp
+++
@@ -1,6 +0,0 @@
-//源文件example1_02.cpp的代码，调用Greet函数
-#include "greet.h"
-int main()
-{	Greet();
-     return 0;
-}
---
+++ 第1章_面向对象及C++语言概述/example1_01.cpp
@@ -0,0 +1,8 @@
+//example1_01.cpp：单文件程序的实现方法示例
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout<<"Hello world!\n";
+	return 0;
+}
